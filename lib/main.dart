7781c110eb221e8df8fcb746862fd880349d986e
gcolmekci <gcolmekci@gmail.com>
2022-06-28 18:46:44 +0300

    some bug fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:school_project/authentication/register.dart';
 import 'package:school_project/authentication/welcome.dart';
 import 'package:school_project/const_files/colors.dart';
 
@@ -16,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'School Project',
+      title: 'Kütüphane App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
